2f73a38b4c4b100fb9497aba39196b2fab28d7b7
Rouzbeh-Berenji <berenji.ro@gmail.com>
2023-06-24 00:52:02 -0400

    defining the slot machine spin function

--- SlotMachineGame/spinGame.py
+++ SlotMachineGame/spinGame.py
@@ -1,6 +1,52 @@
+import random  # we need some random number fot slot machine
 MAX_LINES = 3  # Write in Capital because it is CONSTANT 
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+
+# we need some symbols for slot machine
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):  # how to generate the items that going to be in our slot machine
+    #randomly pick the number of rows inside of each columns
+    all_symbols =[] 
+    for symbol, symbol_count in symbols.items():  # to iteration trough a dictionary : .item() gives you both the key and a value associated with a dictionary
+        for _ in range(symbol_count) : # "_" is an anonymous variable that you can look trough something 
+            all_symbols.append(symbol)
+
+    columns = [] #defining our columns list
+    #we want to generate a column for every single column that we have
+    for _ in range(cols): 
+        column= []
+        current_symbols = all_symbols[:] # make a copy of all_symbols to current
+        for _ in range (rows):
+             value = random.choice(current_symbols) #picking random valus for each rows / this picks a random value from the list of symbols that we have
+             current_symbols.remove(value)
+             column.append(value) # add this value to our coulmn
+        columns.append(column)
+    return columns
+    
+def print_slot_machines (columns): # our columns in a list (horizontal) we need to rotate them to correctly shown them as a coulmn (verticaly)
+# this operation reffered to as "transposing" (flip our coulmns from horizontal to vertical):
+    for row in range(len(columns[0])):  # we look trough every single row that we have
+        for i, column in enumerate(columns) : # for every single row, we look r=trough every column, and print only the first value
+            if i != len(columns) - 1:# the max index we have
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
+  
+
+
 
 def deposit():
     while True:
@@ -55,5 +101,8 @@ def main():
       else: 
           break
     print(f"You are betting ${bet} on ${lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin (ROWS, COLS, symbol_count)
+    print_slot_machines(slots)
  
 main()
