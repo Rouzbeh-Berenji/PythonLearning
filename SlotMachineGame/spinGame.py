b7a1f6a089d87ee15f05c8a5604922360943b443
Rouzbeh-Berenji <berenji.ro@gmail.com>
2023-06-24 00:37:38 -0400

    adding bet function

--- SlotMachineGame/spinGame.py
+++ SlotMachineGame/spinGame.py
@@ -1,5 +1,6 @@
 MAX_LINES = 3  # Write in Capital because it is CONSTANT 
-
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -16,7 +17,7 @@ def deposit():
 def get_number_of_lines():
     while True:
         lines = input(
-            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ") 
+            "Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")  # a way to put variable between string 
         if lines.isdigit():
             lines = int(lines)
             if 1 <= lines <= MAX_LINES: #to check the values in between two values
@@ -28,10 +29,31 @@ def get_number_of_lines():
 
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.") # another way to put variable between string 
+        else:
+            print("Please enter a number.")
+
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print ( balance, lines)
+    while True: 
+      bet = get_bet()
+      total_bet = bet*lines
 
+      if total_bet > balance:  #check to ensure that the requested bet amount is enough based on the current balance !
+          print( f"You don't have enough to bet that amount, your current balance is ${balance}")
+      else: 
+          break
+    print(f"You are betting ${bet} on ${lines} lines. Total bet is equal to: ${total_bet}")
+ 
 main()
